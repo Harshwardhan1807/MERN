fddf60b2a371091baa0955538d312974bd9da6d6
Harshwardhan <harshwardhan.sawant@baseel.com>
2025-02-15 15:16:56 +0530

    Product Model

---
+++ backend/models/ProductModel.js
@@ -0,0 +1,45 @@
+const mongoose = require("mongoose")
+const productSchema = mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    description: {
+        type: String,
+        required: true,
+    },
+    category: {
+        type: String,
+        required: true,
+    },
+    count: {
+        type: Number,
+        required: true,
+    },
+    price: {
+        type: Number,
+        required: true,
+    },
+    rating: {
+        type: Number,
+    },
+    reviewsNumber: {
+        type: Number,
+    },
+    sales: {
+        type: Number,
+        default: 0
+    },
+    attrs: [
+        {key: {type: String}, value: {type: String}}
+    ],
+    images: [],
+    reviews: []
+}, {
+    timestamps: true,
+})
+productSchema.index()
+const Product = mongoose.model("Product", productSchema)
+
+module.exports = Product
