627f1f00a6853df91e4d0e35db2ac313a0ecec36
Harshwardhan <harshwardhan.sawant@baseel.com>
2025-06-30 19:56:01 +0530

    Section 11 completed

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,13 @@
+const { createServer } = require("http");
+const { Server } = require("socket.io");
 const express = require("express");
 const fileUpload = require("express-fileupload");
 const cokkieParser = require("cookie-parser");
 const app = express();
-const port = 5000;
+
+const httpServer = createServer(app);
+global.io = new Server(httpServer)
+
 
 app.use(express.json());
 app.use(cokkieParser());
@@ -35,7 +40,8 @@ app.use((error, req, res, next) => {
     }
 });
 
-app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`);
+const PORT = process.env.PORT || 5000;
+httpServer.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
 });
 
